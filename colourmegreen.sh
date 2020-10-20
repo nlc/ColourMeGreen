67980ed1f42f802ecceaace78c2a5888f2996cbf
Nate <lander@kicksite.net>
2020-10-20 18:13:51 -0500

    First commit

---
+++ colourmegreen.sh
@@ -0,0 +1,48 @@
+spaceline() {
+  echo
+  echo
+  echo -n "[A"
+}
+
+echo "CTHULHU!"
+sleep 1
+echo -e "[32;2m[8D[A#3CTHULHU!\n#4CTHULHU![0m"
+sleep 1
+
+# riff
+
+spaceline
+echo "[2;35;1mViolet[0m flows from the wound in your chest[0m"
+sleep 1
+spaceline
+echo "[7;1mBlack[0m is the hole in which you rest"
+sleep 1
+spaceline
+echo "Your heart of [33;2;7mgold[0m was ripped in two"
+sleep 1
+spaceline
+echo "Soaked in the [32;1msi[0m[32mck[0m[32;2;1mne[0m[32;2mss[0m that is you"
+sleep 1
+
+spaceline
+echo "[7;2mGrey[0m is the box that holds my head"
+sleep 1
+spaceline
+echo "[33mYellow[0m's the wind when everyone's dead"
+sleep 1
+spaceline
+echo "[31;2mRed[0m is the blood dried on the rope"
+sleep 1
+spaceline
+echo "But [32mGREEN[6D[AGREEN[3D[2BGREEN[A[D[0m is your last hope"
+sleep 1
+
+spaceline
+
+echo "(Colour me green) is the colour of my god"
+# 
+# (instrumental)
+# 
+# (Colour me green) is the colour of my god[x4]
+# 
+# (Colour me green) green is the colour of my god[x4]
